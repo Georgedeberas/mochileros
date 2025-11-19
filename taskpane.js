c8cf1e93b172f649054713afcbcb78aab502e46a
Goerge de Beras <georgeberas@hotmail.com>
2025-11-19 19:04:14 -0400

    Update taskpane.js

--- taskpane.js
+++ taskpane.js
@@ -1,1 +1,11 @@
+/* global Office, console */
 
+Office.onReady((info) => {
+    // Verificamos si estamos dentro de Excel
+    if (info.host === Office.HostType.Excel) {
+        console.log("Mochileros RD: Entorno de Office cargado correctamente.");
+        document.getElementById("status-message").innerText = "Sistema listo.";
+    } else {
+        console.error("Mochileros RD: Error, no se detectó el host de Excel.");
+    }
+});
